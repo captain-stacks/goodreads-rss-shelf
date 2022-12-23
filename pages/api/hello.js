f92ab23301efbd5e83fac311f714852f3e921eea
Jacob Spevak <Jacob.Spevak@fdmgroup.com>
2022-12-23 17:09:04 -0600

    initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const url = 'https://goodreads.com/review/list_rss/' + req.query.url
+  const text = await fetch(url).then(res => res.text())
+  res.send(text)
 }
